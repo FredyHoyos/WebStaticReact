22eeac1e1e9257ab5585dd33bcefa85b0a180290
unknown <fredy.hoyos1@udea.edu.co>
2025-05-05 10:00:01 -0500

    Pruebas vercel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   <section>
    <div className=' flex flex-col items-center w-full'>
     <Hero />
-    <Button className="bg-blue-500 hover:bg-blue-600">Mi primer CI-CD</Button>
+    <Button className="bg-blue-500 hover:bg-blue-600">Hecho por fredy</Button>
     <Feactures />
     <div className='flex flex-col items-center w-full'>
       <BlogCard />
